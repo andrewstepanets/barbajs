98584356f87a52fc09cbe8ba77f9c0219539b304
Andrii Stepanets <ovrass@gmail.com>
2020-10-04 23:49:37 +0300

    Using Barba CSS and once hook

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,25 @@
 
+import barba from '@barba/core';
+import barbaCss from '@barba/css';
+
+// tell Barba to use the css plugin
+barba.use(barbaCss);
+
+// init Barba
+barba.init({
+    transitions: [
+        {
+            name: 'home',
+            beforeOnce(){
+                console.log('beforeOnce');
+            },
+            // with css plugin, this will not run
+            once(){
+                console.log('once');
+            },
+            afterOnce(){
+                console.log('afterOnce');
+            }
+        }
+    ]
+});
