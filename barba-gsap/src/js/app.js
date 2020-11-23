9e011612c975029dbabe335b44125b09bec2776b
Andrii Stepanets <ovrass@gmail.com>
2020-11-23 13:21:59 +0200

    Created intro GSAP

--- barba-gsap/src/js/app.js
+++ barba-gsap/src/js/app.js
@@ -1,3 +1,21 @@
 import barba from '@barba/core';
 import gsap from 'gsap';
 
+const animationEnter = (container) => {
+    return gsap.from(container, { autoAlpha: 0, duration: 2, clearProps: 'all', ease: 'none'});
+}
+const animationLeave = (container) => {
+    return gsap.to(container, { autoAlpha: 0, duration: 2, clearProps: 'all', ease: 'none' });
+}
+barba.init({
+    transitions: [
+        {
+            once({ next }){
+                animationEnter(next.container);
+            },
+            leave({ current }){
+                animationEnter(current.container);
+            }
+        }
+    ]
+})
