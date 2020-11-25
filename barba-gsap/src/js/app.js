f3a3e54b8678b132ea24e1967ca6b666684d27ee
Andrii Stepanets <ovrass@gmail.com>
2020-11-25 18:52:22 +0200

    Animated enter timeline

--- barba-gsap/src/js/app.js
+++ barba-gsap/src/js/app.js
@@ -1,13 +1,25 @@
 import barba from '@barba/core';
+import gsap from 'gsap/gsap-core';
 import  { animationEnter, animationLeave } from './animations';
 
-
+const resetActiveLink = () => gsap.set('a.is-active span', {
+    xPercent: -100,
+    transformOriginal: 'left'
+})
 
 barba.init({
     transitions: [
         {
             once({ next }){
-                animationEnter(next.container);
+                resetActiveLink();
+                gsap.from('header a', {
+                    duration: 0.6,
+                    yPercent: 100,
+                    stagger: 0.2,
+                    ease: 'power1.out',
+                    onComplete: () => animationEnter(next.container)
+                })
+                // animationEnter(next.container);
             },
             leave: ({ current }) => animationLeave(current.container),
             enter({ next }) {
